6e0cc3d6dec7463ad644eed16e02bf58bb830794
何志坚 <1305696249@qq.com>
2021-05-01 16:02:56 +0800

    feat: 新增自动上传github剧本

---
+++ build/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn docs:build
+
+# 复制dockerFile文件和nginx配置文件
+cp Dockerfile ../note/docs/.vuepress/dist
+cp default.conf ../note/docs/.vuepress/dist
+
+# 进入生成的文件夹
+cd note/docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:AJianNote/vue-study-note.git master:gh-pages
